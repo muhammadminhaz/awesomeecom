6307f6e10b32a756b4c0836183eea5f4eaee7159
Minhaz <mminhazurrahman0@gmail.com>
2025-12-24 11:58:51 +0600

    Initialization - API Gateway

---
+++ api-gateway/src/test/java/com/muhammadminhaz/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammadminhaz.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
