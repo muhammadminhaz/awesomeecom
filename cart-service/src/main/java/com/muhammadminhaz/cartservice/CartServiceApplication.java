89ca19dea026f32c99cf33a9f4e6f2a9e15d0298
Minhaz <mminhazurrahman0@gmail.com>
2025-12-24 14:15:12 +0600

    Initialization - Eureka Server Service Discovery

--- cart-service/src/main/java/com/muhammadminhaz/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/muhammadminhaz/cartservice/CartServiceApplication.java
@@ -3,7 +3,10 @@ package com.muhammadminhaz.cartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CartServiceApplication {
 
     public static void main(String[] args) {
